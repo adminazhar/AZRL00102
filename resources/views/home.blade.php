a16cade7204213ed04e97f4a68b06ad2b26ce9b5
Azhar Khan <admin@azhark.com>
2020-12-07 20:31:45 +0530

    login functionality updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,42 @@
-@extends('layouts.master')
+@extends('layouts.blank')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="app-content content ">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
             </div>
-            <a href="{{ route('logout') }}"
-               onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
+            <div class="content-body">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="card-header">{{ __('Dashboard') }}</div>
 
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
+                            <div class="card-body">
+                                @if (session('status'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
+
+                                {{ __('You are logged in!') }}
+                            </div>
+                        </div>
+                        <a href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
-</div>
+
 @endsection
