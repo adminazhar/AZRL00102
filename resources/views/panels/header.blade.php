8e740436de55bc5125cda3713ad63b02b520d002
Azhar Khan <admin@azhark.com>
2020-12-12 16:54:59 +0530

    updated blade files

--- resources/views/panels/header.blade.php
+++ resources/views/panels/header.blade.php
@@ -6,33 +6,9 @@
                                                                                                    data-feather="menu"></i></a>
                 </li>
             </ul>
-            <ul class="nav navbar-nav bookmark-icons">
-                <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-email.html" data-toggle="tooltip"
-                                                          data-placement="top" title="Email"><i class="ficon"
-                                                                                                data-feather="mail"></i></a>
-                </li>
-                <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-chat.html" data-toggle="tooltip"
-                                                          data-placement="top" title="Chat"><i class="ficon"
-                                                                                               data-feather="message-square"></i></a>
-                </li>
-                <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-calendar.html"
-                                                          data-toggle="tooltip" data-placement="top" title="Calendar"><i
-                            class="ficon" data-feather="calendar"></i></a></li>
-                <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-todo.html" data-toggle="tooltip"
-                                                          data-placement="top" title="Todo"><i class="ficon"
-                                                                                               data-feather="check-square"></i></a>
-                </li>
-            </ul>
+
             <ul class="nav navbar-nav">
-                <li class="nav-item d-none d-lg-block"><a class="nav-link bookmark-star"><i class="ficon text-warning"
-                                                                                            data-feather="star"></i></a>
-                    <div class="bookmark-input search-input">
-                        <div class="bookmark-input-icon"><i data-feather="search"></i></div>
-                        <input class="form-control input" type="text" placeholder="Bookmark" tabindex="0"
-                               data-search="search">
-                        <ul class="search-list search-list-bookmark"></ul>
-                    </div>
-                </li>
+                <li class="nav-item d-none d-lg-block">&nbsp;</li>
             </ul>
         </div>
         <ul class="nav navbar-nav align-items-center ml-auto">
